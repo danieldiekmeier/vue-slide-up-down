064f0913fe18e368bc88ea42e1286e27ed69e6b5
Nikita Nafranets <eddimensi@gmail.com>
2018-06-27 15:47:47 +0300

    Fix bug with jumping content and little refactoring

--- src/slide-up-down.js
+++ src/slide-up-down.js
@@ -5,77 +5,56 @@ export default {
     active: Boolean,
     duration: {
       type: Number,
-      default: 500
+      default: 500,
     },
     tag: {
       type: String,
-      default: 'div'
-    }
+      default: 'div',
+    },
   },
 
   data: () => ({
-    maxHeight: 0,
-    offsetHeight: 0,
-    isMounted: false
+    scrollHeight: 0,
   }),
 
-  render (h) {
+  render(h) {
     return h(
       this.tag,
       {
         style: this.style,
-        ref: 'container'
+        ref: 'container',
       },
-      this.$slots.default
-    )
+      this.$slots.default,
+    );
   },
 
-  mounted () {
-    this.layout()
-
-    window.addEventListener('resize', this.layout)
-
-    setTimeout(() => {
-      this.isMounted = true
-    }, 0)
+  async mounted() {
+    window.addEventListener('resize', this.getHeight); // recalc height on resize window
+    await this.$nextTick();
+    this.getHeight();
   },
 
-  destroyed () {
-    window.removeEventListener('resize', this.layout)
-  },
-
-  watch: {
-    active () {
-      this.layout()
-    }
+  destroyed() {
+    window.removeEventListener('resize', this.getHeight);
   },
 
   computed: {
-    style () {
+    style() {
+      const heightSize = this.active ? this.scrollHeight : 0;
+
       return {
         overflow: 'hidden',
         'transition-property': 'height',
-        height: this.isMounted ? this.maxHeight + 'px' : 'auto',
-        'transition-duration': this.duration + 'ms'
-      }
-    }
+        height: `${heightSize}px`,
+        'transition-duration': `${this.duration}ms`,
+      };
+    },
   },
 
   methods: {
-    layout () {
-      const { container } = this.$refs
-
-      if (this.active) {
-        const style = container.getAttribute('style')
-        container.removeAttribute('style')
-        this.maxHeight = container.offsetHeight
-        container.setAttribute('style', style)
-
-        // call this explicitely to force a new layout
-        this.offsetHeight = container.offsetHeight
-      } else {
-        this.maxHeight = 0
-      }
-    }
-  }
-}
+    getHeight() {
+      const { container } = this.$refs;
+      this.scrollHeight = container.scrollHeight;
+    },
+  },
+};
