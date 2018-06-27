b0a49015ff9f37d0878540458437c2adef617d7a
Nikita Nafranets <eddimensi@gmail.com>
2018-06-27 16:23:17 +0300

    Add eslint config based on standard style

--- src/slide-up-down.js
+++ src/slide-up-down.js
@@ -5,56 +5,56 @@ export default {
     active: Boolean,
     duration: {
       type: Number,
-      default: 500,
+      default: 500
     },
     tag: {
       type: String,
-      default: 'div',
-    },
+      default: 'div'
+    }
   },
 
   data: () => ({
-    scrollHeight: 0,
+    scrollHeight: 0
   }),
 
-  render(h) {
+  render (h) {
     return h(
       this.tag,
       {
         style: this.style,
-        ref: 'container',
+        ref: 'container'
       },
-      this.$slots.default,
-    );
+      this.$slots.default
+    )
   },
 
-  async mounted() {
-    window.addEventListener('resize', this.getHeight); // recalc height on resize window
-    await this.$nextTick();
-    this.getHeight();
+  async mounted () {
+    window.addEventListener('resize', this.getHeight) // recalc height on resize window
+    await this.$nextTick()
+    this.getHeight()
   },
 
-  destroyed() {
-    window.removeEventListener('resize', this.getHeight);
+  destroyed () {
+    window.removeEventListener('resize', this.getHeight)
   },
 
   computed: {
-    style() {
-      const heightSize = this.active ? this.scrollHeight : 0;
+    style () {
+      const heightSize = this.active ? this.scrollHeight : 0
 
       return {
         overflow: 'hidden',
-        'transition-property': 'height',
+        transitionProperty: 'height',
         height: `${heightSize}px`,
-        'transition-duration': `${this.duration}ms`,
-      };
-    },
+        transitionDuration: `${this.duration}ms`
+      }
+    }
   },
 
   methods: {
-    getHeight() {
-      const { container } = this.$refs;
-      this.scrollHeight = container.scrollHeight;
-    },
-  },
-};
+    getHeight () {
+      const { container } = this.$refs
+      this.scrollHeight = container.scrollHeight
+    }
+  }
+}
