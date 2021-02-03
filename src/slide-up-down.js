55a7749b9d30b8bcb3f092b21af599831324cbf7
Egor Lazuka (Home) <lazuka_99@mail.ru>
2021-02-03 11:33:29 +0300

    Vue 3 support

--- src/slide-up-down.js
+++ src/slide-up-down.js
@@ -1,6 +1,7 @@
-export default {
-  name: 'SlideUpDown',
+import { h, ref, computed, watch, nextTick, onMounted } from 'vue'
 
+export default {
+  name: 'OmegaSlideUpDown',
   props: {
     active: Boolean,
     duration: {
@@ -16,113 +17,106 @@ export default {
       default: true,
     },
   },
+  setup(props, { emit, slots }) {
+    // State
+    const initial = ref(false)
+    const hidden = ref(!props.active)
+    const style = ref({})
+    const forceUpdate = ref(null)
+    const rootComponent = ref(null)
 
-  data: () => ({
-    style: {},
-    initial: false,
-    hidden: false,
-  }),
-
-  watch: {
-    active() {
-      this.layout()
-    },
-  },
-
-  render(h) {
-    return h(
-      this.tag,
-      {
-        style: this.style,
-        attrs: this.attrs,
-        ref: 'container',
-        on: { transitionend: this.onTransitionEnd },
-      },
-      this.$slots.default
-    )
-  },
-
-  mounted() {
-    this.layout()
-    this.initial = true
-  },
-
-  created() {
-    this.hidden = !this.active
-  },
-
-  computed: {
-    el() {
-      return this.$refs.container
-    },
-
-    attrs() {
+    // Computed
+    const attrs = computed(() => {
       const attrs = {
-        'aria-hidden': !this.active,
-        'aria-expanded': this.active,
+        'aria-hidden': !props.active,
+        'aria-expanded': props.active,
       }
 
-      if (this.useHidden) {
-        attrs.hidden = this.hidden
+      if (props.useHidden) {
+        attrs.hidden = hidden
       }
 
       return attrs
-    },
-  },
+    })
 
-  methods: {
-    layout() {
-      if (this.active) {
-        this.hidden = false
-        this.$emit('open-start')
-        if (this.initial) {
-          this.setHeight('0px', () => this.el.scrollHeight + 'px')
-        }
+    // Methods
+    const onTransitionEnd = event => {
+      if (event.target !== rootComponent.value) return
+
+      if (props.active) {
+        style.value = {}
+        emit('open-end')
       } else {
-        this.$emit('close-start')
-        this.setHeight(this.el.scrollHeight + 'px', () => '0px')
-      }
-    },
-
-    asap(callback) {
-      if (!this.initial) {
-        callback()
-      } else {
-        this.$nextTick(callback)
-      }
-    },
-
-    setHeight(temp, afterRelayout) {
-      this.style = { height: temp }
-
-      this.asap(() => {
-        // force relayout so the animation will run
-        this.__ = this.el.scrollHeight
-
-        this.style = {
-          height: afterRelayout(),
-          overflow: 'hidden',
-          'transition-property': 'height',
-          'transition-duration': this.duration + 'ms',
-        }
-      })
-    },
-
-    onTransitionEnd(event) {
-      // Don't do anything if the transition doesn't belong to the container
-      if (event.target !== this.el) return
-
-      if (this.active) {
-        this.style = {}
-        this.$emit('open-end')
-      } else {
-        this.style = {
+        style.value = {
           height: '0',
           overflow: 'hidden',
         }
-        this.hidden = true
-        this.$emit('close-end')
+        hidden.value = true
+        emit('close-end')
       }
-    },
+    }
+
+    const asap = callback => {
+      if (!initial.value) {
+        callback()
+      } else {
+        nextTick(callback)
+      }
+    }
+
+    const setHeight = (temp, afterRelayout) => {
+      style.value = { height: temp }
+
+      asap(() => {
+        forceUpdate.value = rootComponent.value.scrollHeight
+
+        style.value = {
+          height: afterRelayout(),
+          overflow: 'hidden',
+          'transition-property': 'height',
+          'transition-duration': props.duration + 'ms',
+        }
+      })
+    }
+
+    const layout = () => {
+      if (props.active) {
+        hidden.value = false
+        emit('open-start')
+        if (initial.value) {
+          setHeight('0px', () => rootComponent.value.scrollHeight + 'px')
+        }
+      } else {
+        emit('close-start')
+        setHeight(rootComponent.value.scrollHeight + 'px', () => '0px')
+      }
+    }
+
+    // Watches
+    watch(
+      () => props.active,
+      () => layout()
+    )
+
+    // Hooks
+    onMounted(() => {
+      layout()
+      initial.value = true
+    })
+
+    // Render
+    return () =>
+      h(
+        props.tag,
+        {
+          ...attrs.value,
+          style: style.value,
+          ref: rootComponent,
+          onTransitionEnd,
+        },
+        {
+          default: slots.default,
+        }
+      )
   },
 }
